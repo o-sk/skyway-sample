0e6065ec5152ed7bb97f46e2c0308e78ea7798c0
o-sk <m.ohsuka@gmail.com>
2020-11-16 14:24:24 +0900

    Get peer.id

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React from 'react';
+import Peer from 'skyway-js';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      localStream: null,
+      myId: null,
+    };
+  }
+
   componentDidMount() {
     navigator.mediaDevices.getUserMedia({video: true, audio: true})
       .then( stream => {
       const videoElm = document.getElementById('my-video');
       videoElm.srcObject = stream;
       videoElm.play();
-      this.stream = stream;
+      this.setState({localStream: stream})
     }).catch( error => {
       console.error('mediaDevice.getUserMedia() error:', error);
       return;
+    });
+
+    const peer = new Peer({
+      key: process.env.REACT_APP_SKYWAY_API_KEY,
+      debug: 3
+    });
+    peer.on('open', () => {
+      this.setState({myId: peer.id})
     });
   }
 
@@ -19,6 +36,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <video id="my-video" width="400px" autoPlay muted playsInline></video>
+        <p id="my-id">{this.state.myId}</p>
       </div>
     );
   }
